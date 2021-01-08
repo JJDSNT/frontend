e085db3d5b8f252f5f560b6dac43513002d234ce
Jaime Dias <jaime.dias@gmail.com>
2021-01-07 21:51:46 -0300

    mockdata

--- js/estados.js
+++ js/estados.js
@@ -1,6 +1,35 @@
 // States and Cities mockup Data
 const data = {
-  name: 'John Doe',
-  username: 'johndoe',
-  email: 'jdoe@gmail.com'
+  'estados':[
+    {
+      'id':1,
+      'code':'BA',
+      'name':'Bahia',
+      'cities':[
+        {
+          'code':1,
+          'name':'Salvador'
+        },
+        {
+          'code':2,
+          'name':'Vitoria da Conquista'
+        }
+      ]
+    },
+    {  
+      'id':2,
+      'code':'SP',
+      'name':'São Paulo',
+      'cities':[
+        {
+          'code':3,
+          'name':'São Paulo'
+        },
+        {
+          'code':4,
+          'name':'Itú'
+        }
+      ]
+    }
+  ]
 }
